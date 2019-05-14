0eceb55d17c388cdc1320a27b2ce42d153a19ba4
JoseChan <524233828@qq.com>
2019-05-14 23:51:29 +0800

    fix

--- resources/views/web/menu.blade.php
+++ resources/views/web/menu.blade.php
@@ -1,5 +1,5 @@
 <div class="menu">
     @foreach($menus as $menu)
-    <li @if($menu->link == url()->current()) class="select" @endif onclick="location.href = '{{$menu->link}}'">{{$menu->name}}</li>
+    <li @if($menu->link == request()->fullUrl()) class="select" @endif onclick="location.href = '{{$menu->link}}'">{{$menu->name}}</li>
     @endforeach
 </div>
