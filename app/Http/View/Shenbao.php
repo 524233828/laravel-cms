a24c62730db4f322d606009844a77840a2926265
JoseChan <524233828@qq.com>
2019-05-15 17:47:32 +0800

    fix

--- app/Http/View/Shenbao.php
+++ app/Http/View/Shenbao.php
@@ -19,9 +19,47 @@ class Shenbao extends AbstractViewable
 
     protected $is_leaf = true;
 
+    public function __construct()
+    {
+        $this->addScript($this->script());
+    }
 
     public function render()
     {
         return view($this->view);
+    }
+
+    protected function script(){
+        return <<<SCRIPT
+    $(window).scroll(function (){
+        var topScroll=getScroll();
+        var topDiv="100px";
+        var top=topScroll+parseInt(topDiv);
+        var mintop = $(".container .content").offset().top + 10;
+        var maxtop = $(".container .footer").offset().top - $("#shenbao").height() -10;
+        if(top<mintop){
+            top=mintop;
+        }
+        if(top>maxtop){
+            top = maxtop;
+        }
+
+        $("#shenbao").stop(true).animate({"top":top},50);
+        
+    })
+    function getScroll(){
+        var bodyTop = 0;
+        if (typeof window.pageYOffset != 'undefined') {
+            bodyTop = window.pageYOffset;
+        } else if (typeof document.compatMode != 'undefined' && document.compatMode != 'BackCompat') {
+            bodyTop = document.documentElement.scrollTop;
+        }
+        else if (typeof document.body != 'undefined') {
+            bodyTop = document.body.scrollTop;
+        }
+        return bodyTop
+    }
+SCRIPT;
+
     }
 }
