39b0b653d5c704b3134b3b65d042e7f6d8af55cc
JoseChan <524233828@qq.com>
2019-05-15 14:11:27 +0800

    fix

--- resources/views/web/module-10.blade.php
+++ resources/views/web/module-10.blade.php
@@ -1,6 +1,10 @@
 <div class="module-10">
     <div class="title">{{$title}}</div>
     <div class="list">
+
+        @if($pagination->isEmpty())
+            <div style="width:100%; text-align: center;color: #999999;height: 40px;line-height: 40px">暂无更多文章</div>
+        @endif
         @foreach($pagination as $news)
         <a href="{{url("/detail?id={$news['id']}")}}" class="item-contain">
             <div class="news-title">{{$news['title']}}</div>
