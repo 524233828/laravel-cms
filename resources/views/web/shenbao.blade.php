f982b13a8e8ada3607ce0c165c95e50e291a1cd9
JoseChan <524233828@qq.com>
2019-06-30 10:47:59 +0800

    fix

--- resources/views/web/shenbao.blade.php
+++ resources/views/web/shenbao.blade.php
@@ -11,26 +11,26 @@
 
 <div id="shenbao2">
     <div class="enter-contain">
-        <a href="http://pro.gdstc.gov.cn/egrantweb/">
+        <a href="http://pro.gdstc.gov.cn/egrantweb/" target="_blank">
             <div class="enter-img">
                 <img src="images/computer.png">
                 <div class="enter-cn">广东省科技<br />业务管理<br />阳光政务平台</div>
                 <div class="enter-en">Declaration system</div>
             </div>
         </a>
-        <a href="http://www.gdbs.gov.cn/portal">
+        <a href="http://www.gdbs.gov.cn/portal" target="_blank">
             <div class="enter-img">
                 <div class="enter-cn">广东政务<br />服务网</div>
                 <div class="enter-en">Declaration system</div>
             </div>
         </a>
-        <a href="http://183.60.177.201:8899/manage/login.jsp">
+        <a href="http://183.60.177.201:8899/manage/login.jsp" target="_blank">
             <div class="enter-img">
                 <div class="enter-cn">广东省<br />专业镇认定<br />业务管理系统</div>
                 <div class="enter-en">Declaration system</div>
             </div>
         </a>
-        <a href="https://210.76.73.15/portal/">
+        <a href="https://210.76.73.15/portal/" target="_blank">
             <div class="enter-img">
                 <div class="enter-cn">广东省工业企业<br>技术改造<br>监测系统</div>
                 <div class="enter-en">Declaration system</div>
